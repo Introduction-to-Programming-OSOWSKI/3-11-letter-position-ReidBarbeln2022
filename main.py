e3df243db1e13802f3355076672744f8f855502f
ReidBarbeln2022 <814684@mystma.org>
2021-02-04 16:31:43 +0000

    letterPos try 1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#WRITE YOUR CODE IN THIS FILE
+def letterPos(w, x):
+   
+    for i in range(0, len(w)):
+        
+        if w[i] == x:
+            
+            return i + 1 
+    return 0
+
+print (letterPos("sunshine", "b"))
